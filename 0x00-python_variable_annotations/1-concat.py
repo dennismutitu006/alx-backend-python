f078b4017b404b6bed0e7ce1e1d3b54df7ebab6f
denniswachira <dennismutitu06@gmail.com>
2024-06-06 16:23:30 +0000

    basic notations-concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+a type-annotated function that adds to strings.
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """This function will concatenate two strings"""
+    return str1 + str2
