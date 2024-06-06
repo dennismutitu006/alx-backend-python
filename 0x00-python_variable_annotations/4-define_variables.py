e8bb4ac06528283c114862e2d209f66fa80cf798
denniswachira <dennismutitu06@gmail.com>
2024-06-06 16:58:12 +0000

    defining variables and annotating them

---
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+am defining variables using annoations.
+"""
+
+
+a: int = 1
+"""an integer with the value of 1"""
+
+pi: float = 3.14
+"""pi is a float of 3.14"""
+
+i_understand_annotations: bool = True
+"""A boolean which will return true"""
+
+school: str = "Holberton"
+"""A string indicating the school"""
