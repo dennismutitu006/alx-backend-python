ccf50e2e47656b9e2fada936375d9fb0e60b17a1
DENNIS <dennismutitu06@gmail.com>
2024-06-06 07:43:09 -0700

    i want to leave

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,5 +1,16 @@
 #!/usr/bin/env/ python3
-"""Type annotation of arguments"""
+"""
+Type annotation of arguments
+"""
 def add(a: float, b: float) -> float:
-    """ Add the argument values"""
+    """ Add the argument values
+
+
+    Args:
+        a (float): The first floating-point number.
+        b (float): The second floating point number
+
+    Returns:
+        float: The sum of the floating-point numbers.
+    """
     return a + b
