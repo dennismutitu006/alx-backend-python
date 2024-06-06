26015499626ca321b078958970812cc23a4986e4
denniswachira <dennismutitu06@gmail.com>
2024-06-06 16:46:45 +0000

    convert from float to string using inbuilt func str()

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""
+This function will take a float number and return a sring.
+"""
+
+
+def to_str(n: float) -> str:
+    """ This function will convert a flat to a str"""
+    return str(n)
