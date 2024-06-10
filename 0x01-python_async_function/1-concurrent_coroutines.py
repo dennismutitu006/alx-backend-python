577289f38f94bc5ce0f514538e8f7c28d634a142
denniswachira <dennismutitu06@gmail.com>
2024-06-10 15:07:10 +0000

    multiple coroutines using async.gather

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+'''
+asynchronous func wait_n that calls wait_random multiple times concurrently
+and returns the sorted list of delays.
+'''
+import asyncio
+from typing import List
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    '''Executes wait_random n times.
+
+    Args:
+        n: Number of times to call wait_random.
+        max_delay: The maximum delay for wait_random.
+
+    Returns:
+        A sorted list of the delays.
+    '''
+    #  Use asyncio.gather to run wait_random concurrently for n times
+    wait_times = await asyncio.gather(
+        *tuple(wait_random(max_delay) for _ in range(n)))
+    #  Sort the list of delays
+    return sorted(wait_times)
